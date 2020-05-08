8c665e870337a2998cfdb057b45fe393e443d77a
Alexis Hernandez <alexis22229@gmail.com>
2020-05-07 21:38:25 -0600

    Fix the customize.sh script
    It is placing the sources on the wrong directory.

--- customize.sh
+++ customize.sh
@@ -4,7 +4,13 @@ PACKAGE=$1
 PACKAGE_DIRECTORY=$2
 DEFAULT_PACKAGE="com.alexitc.chromeapp"
 DEFAULT_PACKAGE_DIRECTORY="com/alexitc/chromeapp"
+
+# move files to the new package directory
 mkdir -p src/main/scala/$PACKAGE_DIRECTORY
+mv src/main/scala/$DEFAULT_PACKAGE_DIRECTORY/* src/main/scala/$PACKAGE_DIRECTORY/
+
+# rename packages on files
 find src -name '*' -exec sed -i -e "s/$DEFAULT_PACKAGE/$PACKAGE/g" {} \;
+
+# update packages on build.sbt
 find . -name 'build.sbt' -exec sed -i -e "s/$DEFAULT_PACKAGE/$PACKAGE/g" {} \;
-mv src/main/scala/$DEFAULT_PACKAGE_DIRECTORY src/main/scala/$PACKAGE_DIRECTORY
